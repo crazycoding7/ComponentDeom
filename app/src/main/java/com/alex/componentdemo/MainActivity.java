7fe103dc69b42b58ab62aecd478571b99c78750c
qishuai <644172593@qq.com>
2021-04-19 14:21:01 +0800

    feature: 实现组件单独运行功能 (根据配置文件标识修改).

--- app/src/main/java/com/alex/componentdemo/MainActivity.java
+++ app/src/main/java/com/alex/componentdemo/MainActivity.java
@@ -2,7 +2,11 @@ package com.alex.componentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.alex.c_home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView(){
+        findViewById(R.id.btn_main_jump_home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
